0f56a5b1695ad03a3736d17292ad0214dbf39657
Aaron Spivak <69207934+aspivak5@users.noreply.github.com>
2021-01-18 13:47:33 -0500

    added hbar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,6 +13,7 @@ function metadatachart(sample){
 
         var filtersample = metadata.filter(row => row.id == sample);
         var result = filtersample[0];
+        console.log(result)
 
         var demoinfo = d3.select("#sample-metadata");
 
@@ -28,7 +29,28 @@ metadatachart(944);
 //build function for buildings all charts
 function buildCharts(sample){
     d3.json("samples.json").then(data =>{
-    
+        var sampledata = data.samples
+
+        var filtersample = sampledata.filter(row => row.id == sample);
+        var result = filtersample[0];
+        console.log(result)
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        console.log(otu_ids)
+
+        var barData =[{
+            x: sample_values.slice(0,10).reverse(),
+            y: otu_ids.slice(0, 10).map(id=> `OTU ${id}`).reverse(),
+            text: otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }];
+
+        Plotly.newPlot("bar",barData)
+
 
     })
 }
+buildCharts(941)
